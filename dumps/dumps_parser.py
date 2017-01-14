b1aea56b06a05ce6267670f8ec5a119871af5de4
Владимир <icyfalcon@mail.ru>
2017-01-14 12:28:05 +0500

    test

---
+++ dumps/dumps_parser.py
@@ -0,0 +1,33 @@
+import re
+
+
+def main():
+    fnum = 16
+    with open('dumps.txt', 'r') as f:
+        lines = f.readlines()
+    with open('template.txt', 'r') as f:
+        template = f.read()
+    dump = ''
+    content = ''
+    for line in lines:
+        if line.startswith('Frame'):
+            with open('dump%d.html' % fnum, 'w') as f:
+                f.write(template % (dump, content))
+            fnum += 1
+            dump = ''
+            content = ''
+        if re.match(r'[\da-f]{4}', line):
+            dump += line.split('   ')[0]
+            dump += '\n'
+        else:
+            content += line
+    with open('dump%d.html' % fnum, 'w') as f:
+        f.write(template % (dump, content))
+    fnum += 1
+    dump = ''
+    content = ''
+    print(fnum)
+
+
+if __name__ == '__main__':
+    main()
